c126cbc1d3f01d7f7f8f04ed722aa220c85a2640
antblessing <antblessing@gmail.com>
2016-02-08 17:18:47 -0800

    update

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function() {
   var points = document.getElementsByClassName('point');
-
-  var revealPoint = function(point) {
-      point.style.opacity = 1;
-      point.style.transform = "scaleX(1) translateY(0)";
-      point.style.msTransform = "scaleX(1) translateY(0)";
-      point.style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+  
   forEach(points,revealPoint);
 };
 
